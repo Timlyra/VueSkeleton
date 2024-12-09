3b0dcbb5178268a033ec39fc802805ebd1f1124f
Timlyra <tim@lyrawms.nl>
2024-12-09 20:55:10 +0100

    Application of basic recursion w/ styles

--- src/skeleton.js
+++ src/skeleton.js
@@ -1,8 +1,24 @@
 export default {
     bind(el, binding) {
-        el.style.color = binding.value || 'blue';
+        console.log('skeleton bound', binding, el);
+
+        applyDepthToElements(el, 0);
     },
-    update(el, binding) {
-        el.style.color = binding.value || 'blue';
-    }
 };
+
+function applyDepthToElements(el, depth) {
+    el.setAttribute('skeleton-depth', depth);
+
+    const children = el.children;
+    for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+
+        const backgroundAlpha = Math.min(depth / 100, 1) + 0.05;
+
+        // BG color incrementing in opacity as depth increases
+        child.style.backgroundColor = `rgba(0, 0, 0, ${backgroundAlpha})`;
+        child.style.color = `rgba(0, 0, 0, 0.0)`;
+
+        applyDepthToElements(child, depth + 1);
+    }
+}
